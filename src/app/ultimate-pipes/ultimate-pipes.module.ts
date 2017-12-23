527281e43da386cb07dfb3b93c7525ec6c67e1f2
talyo94 <roytyo@gmail.com>
2017-12-23 22:10:35 +0200

    re-arrange module and added new pipes

--- src/app/ultimate-pipes/ultimate-pipes.module.ts
+++ src/app/ultimate-pipes/ultimate-pipes.module.ts
@@ -1,71 +1,109 @@
 import { NgModule } from '@angular/core';
-import { TimesincePipe } from './time/timesince.pipe';
+
+// arrays
+import { EmptyPipe } from './arrays/empty.pipe';
+import { FilterPipe } from './arrays/filter.pipe';
+import { JoinPipe } from './arrays/join.pipe';
+import { ReverseElementsPipe } from './arrays/reverse-elements.pipe';
+import { TallyPipe } from './arrays/tally.pipe';
+
+// boolean
+import { YesNoPipe } from './boolean/yesno.pipe';
+
+// general
+import { DefaultPipe } from './general/default.pipe';
+import { DefaultIfFalsePipe } from './general/default-if-false.pipe';
+import { FirstPipe } from './general/first.pipe';
+import { LastPipe } from './general/last.pipe'
+
+// numbers
+import { CeilPipe } from './numbers/ceil.pipe';
+import { FloorPipe } from './numbers/floor.pipe';
+import { RandomPipe } from './numbers/random.pipe';
+import { RoundPipe } from './numbers/round.pipe';
+
+// text
+import { CapfirstPipe } from './text/capfirst.pipe';
+import { CountPipe } from './text/count.pipe';
+import { CutPipe } from './text/cut.pipe';
+import { LoremPipe } from './text/lorem.pipe';
+import { LowerPipe } from './text/lower.pipe';
+import { ReversePipe } from './text/reverse.pipe';
+import { ReverseWordsPipe } from './text/reverse-words.pipe';
+import { SlugifyPipe } from './text/slugify.pipe';
 import { TruncateCharsPipe } from './text/truncate-chars.pipe';
 import { TitlePipe } from './text/title.pipe';
 import { TruncateWordsPipe } from './text/truncate-words.pipe';
 import { UpperPipe } from './text/upper.pipe';
-import { LowerPipe } from './text/lower.pipe';
-import { CountPipe } from './text/count.pipe';
-import { YesNoPipe } from './boolean/yesno.pipe';
-import { DefaultPipe } from './general/default.pipe';
-import { DefaultIfFalsePipe } from './general/default-if-false.pipe';
-import { EmptyPipe } from './arrays/empty.pipe';
+
+// time
 import { NowPipe } from './time/now.pipe';
-import { ReversePipe } from './text/reverse.pipe';
-import { ReverseWordsPipe } from './text/reverse-words.pipe';
-import { CutPipe } from './text/cut.pipe';
-import { SlugifyPipe } from './text/slugify.pipe';
-import { JoinPipe } from './arrays/join.pipe';
-import { LastPipe } from './general/last.pipe';
-import { FirstPipe } from './general/first.pipe';
-import { FilterPipe } from './arrays/filter.pipe';
-import { TallyPipe } from './arrays/tally.pipe';
+import { TimesincePipe } from './time/timesince.pipe';
+import { TimeuntilPipe } from './time/timeuntil.pipe';
+
 
 @NgModule({
   declarations: [
-    TruncateCharsPipe,
-    TimesincePipe,
-    TitlePipe,
-    TruncateWordsPipe,
-    UpperPipe,
-    LowerPipe,
-    CountPipe,
+    EmptyPipe,
+    FilterPipe,
+    JoinPipe,
+    ReverseElementsPipe,
+    TallyPipe,
     YesNoPipe,
     DefaultPipe,
     DefaultIfFalsePipe,
-    EmptyPipe,
-    NowPipe,
+    FirstPipe,
+    LastPipe,
+    CeilPipe,
+    FloorPipe,
+    RandomPipe,
+    RoundPipe,
+    CapfirstPipe,
+    CountPipe,
+    CutPipe,
+    LoremPipe,
+    LowerPipe,
     ReversePipe,
     ReverseWordsPipe,
-    CutPipe,
     SlugifyPipe,
-    JoinPipe,
-    LastPipe,
-    FirstPipe,
-    FilterPipe,
-    TallyPipe
+    TruncateCharsPipe,
+    TitlePipe,
+    TruncateWordsPipe,
+    UpperPipe,
+    NowPipe,
+    TimesincePipe,
+    TimeuntilPipe
   ],
   exports: [
-    TruncateCharsPipe,
-    TimesincePipe,
-    TitlePipe,
-    TruncateWordsPipe,
-    UpperPipe,
-    LowerPipe,
-    CountPipe,
+    EmptyPipe,
+    FilterPipe,
+    JoinPipe,
+    ReverseElementsPipe,
+    TallyPipe,
     YesNoPipe,
     DefaultPipe,
     DefaultIfFalsePipe,
-    EmptyPipe,
-    NowPipe,
+    FirstPipe,
+    LastPipe,
+    CeilPipe,
+    FloorPipe,
+    RandomPipe,
+    RoundPipe,
+    CapfirstPipe,
+    CountPipe,
+    CutPipe,
+    LoremPipe,
+    LowerPipe,
     ReversePipe,
     ReverseWordsPipe,
-    CutPipe,
     SlugifyPipe,
-    JoinPipe,
-    LastPipe,
-    FirstPipe,
-    FilterPipe
+    TruncateCharsPipe,
+    TitlePipe,
+    TruncateWordsPipe,
+    UpperPipe,
+    NowPipe,
+    TimesincePipe,
+    TimeuntilPipe
   ]
 })
 export class UltimatePipesModule {
