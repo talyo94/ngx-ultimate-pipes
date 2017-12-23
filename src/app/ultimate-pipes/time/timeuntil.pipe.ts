9e52067d9d8c3c23b88476c859041b1af058433e
talyo94 <roytyo@gmail.com>
2017-12-23 22:03:35 +0200

    added time until
    show time until certain date in human format

---
+++ src/app/ultimate-pipes/time/timeuntil.pipe.ts
@@ -0,0 +1,39 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'timeuntil'
+})
+export class TimeuntilPipe implements PipeTransform {
+
+  transform(value: Date): any {
+    const now: Date = new Date();
+    const timedelta = value.getTime() - now.getTime();
+    if (timedelta <= 0) {
+      return '0';
+    }
+    let x = timedelta / 1000;
+    const seconds = Math.floor(x % 60);
+    x = x / 60;
+    const minutes = Math.floor(x % 60);
+    x = x / 60;
+    const hours = Math.floor(x % 24);
+    x = Math.floor(x / 24);
+
+
+    let t = '';
+    if (x) {
+      t += `${x} days`;
+    }
+    if (hours) {
+      t ? t += `, ${hours} hours ` : t += `${hours} hours`;
+    }
+    if (minutes) {
+      t ? t += `, ${minutes} minutes` : t += `${minutes} minutes`;
+    }
+    if (seconds) {
+      t ? t += `, ${seconds} seconds` : t += `${seconds} seconds`;
+    }
+    return t;
+  }
+
+}
